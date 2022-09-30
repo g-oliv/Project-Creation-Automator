c882b2d5c43a746294d4b978c7f364b1f72ea8f7
Gonçalo Oliveira <goncalo.m.g.oliveira@gmail.com>
2022-09-30 18:26:12 +0100

    Add call from anywhere functionality

--- createProject.sh
+++ createProject.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-function new() {
-    out=$(python createProject.py $@)
+BASEDIR=$(dirname "$0")
 
-if [ $? ]
-then
-    visibility=$out
-    echo $visibility
-    gh repo create $1 --$visibility --clone
-    cd $1
-    touch README.md
-    git add . && git commit -a -m "Initial Commit"
-    git push -u origin main
-fi
+new() {
+
+    OUT=$(python "$BASEDIR"/createProject.py $@)
+
+    if [ $? ]; then
+        VISIBILITY=$OUT
+        gh repo create "$1" --"$VISIBILITY" --clone
+        cd "$1"
+        touch README.md
+        git add . && git commit -a -m "Initial Commit"
+        git push -u origin main
+    fi
+
 }
--- exceptions/missingArgumentException.py
+++ exceptions/missingArgumentException.py
@@ -1,6 +1,6 @@
 class MissingArgumentException(Exception):
 
-    def __init__(self, message="MissingArgumentException: No project name given. Introduce the name of your new project" +
+    def __init__(self, message="MissingArgumentException: No project name given. Introduce the name of your new project\n" +
                  "create [<project name>] [<visibility>]"):
         self.message = message
         super().__init__(self.message)
